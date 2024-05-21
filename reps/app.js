48bc8c78d105591719a70ec28dbf262a3868e1d4
Caio-MacBook <caioa.morais@gmail.com>
2024-05-20 23:55:44 -0300

    xibronhas reps

---
+++ reps/app.js
@@ -0,0 +1,12 @@
+const xibronhas = [1, 2, 3, 4, 5]
+
+console.log(xibronhas[0])
+console.log(xibronhas.length - 1)
+
+for (const xibronha of xibronhas) {
+  console.log(`Xibronha numero: ${xibronha}`)
+}
+
+for (let i = 0; i === xibronhas.length - 1; i++) {
+  console.log(i)
+}
